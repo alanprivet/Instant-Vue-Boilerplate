6418893efea35567a9e3628f84be6113492f9a1d
Alan Privet <chubjr1b@gmail.com>
2020-04-18 01:00:22 +0300

    Basic Boilerplate

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  div.block Instantly
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass">
+@import "./main";
+</style>
